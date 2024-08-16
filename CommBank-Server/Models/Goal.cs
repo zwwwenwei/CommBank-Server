39c089e07df56f3e90031311ba2637ac387a2bff
Wenwei-Zhou <102543390+zwwwenwei@users.noreply.github.com>
2024-08-16 19:35:17 +1000

    update by Wenwei

--- CommBank-Server/Models/Goal.cs
+++ CommBank-Server/Models/Goal.cs
@@ -24,6 +24,7 @@ public class Goal
 
     [BsonRepresentation(BsonType.ObjectId)]
     public List<string>? TagIds { get; set; }
+    public string? Icon { get; set; }
 
     [BsonRepresentation(BsonType.ObjectId)]
     public string? UserId { get; set; }
